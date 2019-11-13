ab7311ed84ff5bf7cca38cb8403f00d0e8dd7742
KhanhTranPC <khanhtrant12@outlook.com>
2019-11-13 13:06:54 +0700

    Object services createObject

---
+++ src/Services/MinIO/MinIO.API/Services/Interfaces/IMinioObjectService.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinIO.API.Services.Interfaces
+{
+    public interface IMinioObjectService
+    {
+        JObject createObject(string bucketName, string objectName, Stream data, long size, string contentType);
+    }
+}
