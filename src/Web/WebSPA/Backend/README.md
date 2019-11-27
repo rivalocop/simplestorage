6db787e9a79a00fd08aecd251698be39bb7f5eff
binhbboong <36161471+binhbboong@users.noreply.github.com>
2019-11-27 15:42:36 +0700

    Revert "api backend"
    This reverts commit a2e349700bbdc7316713917b5cfa5e78e5154ce4.

--- src/Web/WebSPA/Backend/README.md
+++
@@ -1,2 +0,0 @@
-# WebBanHang---Backend
-WebBanHang - Backend
--- src/Web/WebSPA/Backend/routes/product.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const ProductController = require('./../controllers/product.controller');
-
-router.post('/insert-production', ProductController.insert);
-
-module.exports = router;
