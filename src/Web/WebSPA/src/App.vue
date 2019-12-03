46edde7c262b537581d042d70f7735622b1826b4
Dang Thanh Linh <linhdt@gemvietnam.com>
2019-12-03 13:29:12 +0700

    Dashboard UI

--- src/Web/WebSPA/src/App.vue
+++ src/Web/WebSPA/src/App.vue
@@ -9,3 +9,16 @@ export default {
   name: 'App'
 }
 </script>
+
+<style lang="scss">
+  @import "assets/styles/main";
+  @import '../node_modules/material-design-icons/iconfont/material-icons.css';
+  body {
+    scroll-behavior: smooth;
+    #app{
+      background:#F2F4F6;
+      height: 100vh;
+      overflow: auto;
+    }
+  }
+</style>
